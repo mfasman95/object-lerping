21ef3453711a82000356e4a37a09882c8f2d4319
Cody Van De Mark <cavigm@rit.edu>
2017-09-17 23:53:35 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const socketio = require('socket.io');
+const xxh = require('xxhashjs');
+
+const fs = require('fs');
+
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const handler = (req, res) => {
+  fs.readFile(`${__dirname}/../client/index.html`, (err, data) => {
+    // if err, throw it for now
+    if (err) {
+      throw err;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+};
+
+const app = http.createServer(handler);
+const io = socketio(app);
+
+app.listen(PORT);
+
+io.on('connection', (sock) => {
+  const socket = sock;
+  socket.join('room1');
+  
+
+});
+
+console.log(`listening on port ${PORT}`);
